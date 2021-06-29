2f35f23a324fcf0cda96a3da86a27de5d0490f96
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-29 20:45:16 +0530

    [Shreya] Add:  Create a Table

--- EmpPayRollQueries.sql
+++ EmpPayRollQueries.sql
@@ -4,4 +4,12 @@ select * from sys.databases
 
 use Payroll_Service_DB
 
-vggg
+
+create table employee_payroll
+(
+id int not null identity,
+name varchar(25) not null,
+salary money not null,
+start date not null
+);
+select * from employee_payroll
