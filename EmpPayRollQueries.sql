dffb2154fd974f566080ed38574a2ea5cbbc299e
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-30 08:11:25 +0530

    [Shreya] Add:  Insert Data into Database

--- EmpPayRollQueries.sql
+++ EmpPayRollQueries.sql
@@ -12,4 +12,12 @@ name varchar(25) not null,
 salary money not null,
 start date not null
 );
+
+insert into employee_payroll values
+('Shreya',100000.0,'2018-01-03'),
+('Prajakta',300000.0,'2020-05-21'),
+('Ekta',200000.0,'2019-11-13')
+
+
 select * from employee_payroll
+
